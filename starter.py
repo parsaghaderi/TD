70f44fd514e652b76ef45536947d8c913c4d9682
Parsa <parsa@Parsas-MacBook-Air.local>
2021-08-12 08:05:49 -0400

    minor fix

--- starter.py
+++ starter.py
@@ -124,7 +124,7 @@ print(node.neighbors())
 node.VISITED = True
 neighbors = node.neighbors()
 for item in neighbors:
-    print('status of' + item+ ' ' +clientNodeStatus(item))
+    clientNodeStatus(item)
         # print(item)
         # clientNodeUpdate(item, node)
 
